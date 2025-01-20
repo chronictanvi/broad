818b41f33527cbd60b067c0bb7f92b06a6ce048e
yuchi <chronictanvi@gmail.com>
2025-01-20 18:38:33 +0530

    adding fonts (not working)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-}
+  title: "v0 App",
+  description: "Created with v0",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/uql5ydf.css" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
